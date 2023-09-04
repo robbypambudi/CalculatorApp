3d23616e874825b502b3f2bf1bf25c285831bdad
Robby Pambudi <5025211042@mhs.its.ac.id>
2023-09-04 23:04:42 +0700

    first commit

---
+++ Calculator/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonC_Click(object sender, RoutedEventArgs e)
+        {
+            textBoxDisplayNumber.Text = "0";
+        }
+
+        private void NumericButton_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            textBoxDisplayNumber.Text += button.Content.ToString();
+        }
+    }
+}
